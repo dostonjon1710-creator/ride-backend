7412dd64d6848a795966180599fedeadeec25fda
dostonjon1710-creator <dostonjon1710@gmail.com>
2026-04-09 01:07:20 +0200

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,11 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors(); // 👈 TO DODAJEMY
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 }
 bootstrap();
