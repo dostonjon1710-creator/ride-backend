e6cfdcd067a4bec6b265583b06f548b1670790eb
dostonjon1710-creator <dostonjon1710@gmail.com>
2026-04-09 00:29:48 +0200

    Add files via upload

--- app.controller.ts
+++ app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return 'DOSTON BACKEND WORKS 🚀';
+  }
+
+  @Post()
+  createRide(@Body() body: any): string {
+    const { from, to } = body;
+    return `Ride ordered from ${from} to ${to} 🚗`;
   }
 }
